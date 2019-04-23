90d7e159afbc525392b82c155ef391e95e1589d2
Ezequiel Paoloni <epaoloni@certant.com>
2019-04-23 16:49:03 -0300

    Agregadas las clases

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Continente.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Continente.java
@@ -1,7 +1,23 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Continente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
+	
+	private String getNombre() {
+		return this.nombre;
+	}
+	
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
